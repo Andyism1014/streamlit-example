e7c10f8a6c60cc036cea0ebe0aaf97aa72b5757c
Andyism1014 <94820845+Andyism1014@users.noreply.github.com>
2021-12-01 11:09:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,15 @@ import requests
 st.set_page_config(layout="wide")  # this needs to be the first Streamlit command
 
 st.title("Tensor Data Platform")
-
+col1, col2 = st.columns([3, 1])
+  
 # LAT
 #huobi
 r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=100&symbol=latusdt")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
 huobi=pd.DataFrame({"t":a["id"],"symbol":"huobi","Volume":a["amount"]}).iloc[::-1]
 huobi["t"]=pd.to_datetime(huobi["t"],unit="s")
-st.write(huobi)
+col2.write(huobi)
 #okex
 r=requests.get("https://www.okex.com/api/v5/market/history-candles?instId=LAT-USDT&bar=1D")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist(),columns=["id","open","high","low","close","vol","volCcy"])
