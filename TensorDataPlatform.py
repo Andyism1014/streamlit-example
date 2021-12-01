b018f72dfd07143c46683096af65b29dad8a10d5
Andyism1014 <94820845+Andyism1014@users.noreply.github.com>
2021-12-01 15:31:34 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -9,6 +9,7 @@ st.set_page_config(layout="wide")  # this needs to be the first Streamlit comman
 
 st.title("Tensor Data Platform")
 
+
 #LAT
 r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=100&symbol=latusdt")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
@@ -26,7 +27,11 @@ Volume=pd.concat([huobi,okex],ignore_index=True)
 r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=100&symbol=latusdt")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
 Price=pd.DataFrame({"t":pd.to_datetime(a["id"],unit="s"),"Price":(a["high"]+a["low"])/2})
-#pic
+#MovingAverage
+huobi["Volume"]=huobi["Volume"].rolling(14).mean()
+okex["Volume"]=okex["Volume"].rolling(14).mean()
+MAVLAT=pd.concat([a,b])
+#picture
 a=alt.Chart(Price).mark_line().encode(
     x=alt.X("yearmonthdate(t):T",axis=alt.Axis(title=None)),
     y='Price:Q')
@@ -39,6 +44,17 @@ resLAT=alt.layer(a,b).resolve_scale(
     width=800,
     height=350
 ).interactive(bind_y=False)
+c=alt.Chart(MAVLAT).mark_area(opacity=0.6).encode(
+    x=alt.X("yearmonthdate(t):T",axis=alt.Axis(title=None)),
+    y=alt.Y("Volume:Q",axis=alt.Axis(format="s",title="14days Moving Average")),
+    color="symbol:N")
+resLATMAV=alt.layer(a,c).resolve_scale(
+    y = 'independent').properties(
+    width=800,
+    height=300
+).interactive(bind_y=False)
+
+
 
 #CKB
 #binance
@@ -62,19 +78,19 @@ Volume=pd.concat([huobi,bithumb,binance])
 r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=1000&symbol=ckbusdt")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
 Price=pd.DataFrame({"t":pd.to_datetime(a["id"],unit="s"),"Price":(a["high"]+a["low"])/2})
+#MovingAverage
+binance["Volume"]=binance["Volume"].rolling(14).mean()
+bithumb["Volume"]=bithumb["Volume"].rolling(14).mean()
+huobi["Volume"]=huobi["Volume"].rolling(14).mean()
+MAVCKB=pd.concat([binance,bithumb,huobi])
 #picture
 a=alt.Chart(Price).mark_line().encode(
     x=alt.X("yearmonthdate(t):T",axis=alt.Axis(title=None)),
-    y='Price:Q'
-   
-)
+    y='Price:Q')
 b=alt.Chart(Volume).mark_area(opacity=0.6).encode(
     x=alt.X("yearmonthdate(t):T",axis=alt.Axis(title=None)),
     y=alt.Y("Volume:Q",axis=alt.Axis(format="s")),
-    color="symbol:N"
-    
-)
-
+    color="symbol:N")
 resCKB=alt.layer(a,b).resolve_scale(
     y = 'independent').properties(
     width=800,
@@ -88,9 +104,11 @@ right,left= st.columns(2)
 with right:
     st.header("LAT Volume")
     st.write(resLAT)
+    st.write(resLAT)
 with left:
     st.header("CKB Volume")
     st.write(resCKB)
+    st.write(resLAT)
 
 
 
