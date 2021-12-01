4b2fd45b422fb7c985077c08201e7372d9ebb1c8
Andyism1014 <94820845+Andyism1014@users.noreply.github.com>
2021-12-01 11:50:04 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -19,6 +19,13 @@ a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
 huobi=pd.DataFrame({"t":a["id"],"symbol":"huobi","Volume":a["amount"]}).iloc[::-1]
 huobi["t"]=pd.to_datetime(huobi["t"],unit="s")
 
+#okex
+r=requests.get("https://www.okex.com/api/v5/market/history-candles?instId=LAT-USDT&bar=1D")
+a=pd.DataFrame(pd.read_json(r.text)["data"].tolist(),columns=["id","open","high","low","close","vol","volCcy"])
+okex=pd.DataFrame({"t":a["id"],"symbol":"okex","Volume":a["vol"]}).iloc[::-1]
+okex["t"]=pd.to_datetime(okex["t"],unit="ms")
+#concat
+Volume=pd.concat([huobi,okex])
 
 #price
 r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=100&symbol=latusdt")
