53334bcf6f8de9dfa00627f84785911910cf74e4
Andyism1014 <94820845+Andyism1014@users.noreply.github.com>
2021-12-01 15:17:05 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -20,7 +20,7 @@ a=pd.DataFrame(pd.read_json(r.text)["data"].tolist(),columns=["id","open","high"
 a["vol"]=pd.to_numeric(a["vol"])
 okex=pd.DataFrame({"t":a["id"],"symbol":"okex","Volume":a["vol"]}).iloc[::-1]
 okex["t"]=pd.to_datetime(okex["t"],unit="ms")
-#combine
+##concat
 Volume=pd.concat([huobi,okex],ignore_index=True)
 #price
 r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=100&symbol=latusdt")
@@ -34,18 +34,64 @@ b=alt.Chart(Volume).mark_area(opacity=0.6).encode(
     x=alt.X("yearmonthdate(t):T",axis=alt.Axis(title=None)),
     y=alt.Y("Volume:Q",axis=alt.Axis(format="s")),
     color="symbol:N")
-res1=alt.layer(a,b).resolve_scale(
+resLAT=alt.layer(a,b).resolve_scale(
     y = 'independent').properties(
     width=800,
     height=350
 ).interactive(bind_y=False)
+
+#CKB
+#binance
+r=requests.get("https://api.binance.com/api/v3/klines?symbol=CKBUSDT&interval=1d&limit=1000")
+a=pd.read_json(r.text)
+binance=pd.DataFrame({"t":pd.to_datetime(a[0],unit="ms"),"symbol":"binance","Volume":a[5]})
+#bithumb vol in currency
+r=requests.get("https://api.bithumb.com/public/candlestick/CKB_KRW/24h")
+a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
+bithumb=pd.DataFrame({"t":pd.to_datetime(a[0],unit="ms"),"symbol":"bithumb","Volume":a[5]})
+#huobi
+r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=1000&symbol=ckbusdt")
+a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
+huobi=pd.DataFrame({"t":pd.to_datetime(a["id"],unit="s"),"symbol":"huobi","Volume":a["amount"]}).iloc[::-1]
+#gate
+r=requests.get("https://api.gateio.ws/api/v4/spot/candlesticks?currency_pair=CKB_USDT&interval=1d")
+a=pd.read_json(r.text)
+gate=pd.DataFrame({"t":pd.to_datetime(a[0],unit="s"),"symbol":"gate","Volume":a[1]})
+#concat
+Volume=pd.concat([huobi,bithumb,binance,gate])
+#price
+r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=1000&symbol=ckbusdt")
+a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
+Price=pd.DataFrame({"t":pd.to_datetime(a["id"],unit="s"),"Price":(a["high"]+a["low"])/2})
+#picture
+a=alt.Chart(Price).mark_line().encode(
+    x=alt.X("yearmonthdate(t):T",axis=alt.Axis(title=None)),
+    y='Price:Q'
+   
+)
+b=alt.Chart(Volume).mark_area(opacity=0.6).encode(
+    x=alt.X("yearmonthdate(t):T",axis=alt.Axis(title=None)),
+    y=alt.Y("Volume:Q",axis=alt.Axis(format="s")),
+    color="symbol:N"
+    
+)
+
+resCKB=alt.layer(a,b).resolve_scale(
+    y = 'independent').properties(
+    width=800,
+    height=350
+).interactive(bind_y=False)
+
+
 #layouts
 col1, col2, col3 = st.columns(3)
 right,left= st.columns(2)
 with right:
     st.header("LAT Volume")
-    st.write(res1)
-   
+    st.write(resLAT)
+with left:
+    st.header("CKB Volume")
+    st.write(resCKB)
 
 
 
