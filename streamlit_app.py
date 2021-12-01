5b883bae2251f29a68020f6a19a0f9719dbce483
Andyism1014 <94820845+Andyism1014@users.noreply.github.com>
2021-12-01 10:47:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,30 +3,15 @@ import math
 import pandas as pd
 import streamlit as st
 import requests
-from vega_datasets import data
+
 
 st.set_page_config(layout="wide")  # this needs to be the first Streamlit command
 
 st.title("Tensor Data Platform")
 
+r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=100&symbol=latusdt")
+a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
+huobi=pd.DataFrame({"t":a["id"],"symbol":"huobi","Volume":a["amount"]}).iloc[::-1]
+huobi["t"]=pd.to_datetime(huobi["t"],unit="s")
 
-source = data.seattle_weather()
-
-base = alt.Chart(source).encode(
-    alt.X('month(date):T', axis=alt.Axis(title=None))
-)
-
-area = base.mark_area(opacity=0.3, color='#57A44C').encode(
-    alt.Y('average(temp_max)',
-          axis=alt.Axis(title='Avg. Temperature (°C)', titleColor='#57A44C')),
-    alt.Y2('average(temp_min)')
-)
-
-line = base.mark_line(stroke='#5276A7', interpolate='monotone').encode(
-    alt.Y('average(precipitation)',
-          axis=alt.Axis(title='Precipitation (inches)', titleColor='#5276A7'))
-)
-
-alt.layer(area, line).resolve_scale(
-    y = 'independent'
-)
+st.write(huobi)
