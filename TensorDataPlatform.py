787591441ec89dcdefedd582b21a5d85347fe922
Andyism1014 <94820845+Andyism1014@users.noreply.github.com>
2021-12-01 13:38:41 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -13,13 +13,12 @@ st.title("Tensor Data Platform")
 #huobi
 r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=100&symbol=latusdt")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
-huobi=pd.DataFrame({"t":a["id"],"symbol":"huobi","Volume":a["amount"]}).iloc[::-1]
-huobi["t"]=pd.to_datetime(huobi["t"],unit="s")
+huobi=pd.DataFrame({"t":a["id"],"symbol":"huobi","Volume":a["amount"]})
 #okex
 r=requests.get("https://www.okex.com/api/v5/market/history-candles?instId=LAT-USDT&bar=1D")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist(),columns=["id","open","high","low","close","vol","volCcy"])
-okex=pd.DataFrame({"t":a["id"],"symbol":"okex","Volume":a["vol"]}).iloc[::-1]
-okex["t"]=pd.to_datetime(okex["t"],unit="ms")
+okex=pd.DataFrame({"t":a["id"],"symbol":"okex","Volume":a["vol"]})
+
 
 s1 = pd.Series(['a', 'b'])
 s2 = pd.Series(['c', 'd'])
