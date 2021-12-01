8897921ad8771f72d1d683902fcfc7adcb393259
Andyism1014 <94820845+Andyism1014@users.noreply.github.com>
2021-12-01 10:37:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")  # this needs to be the first Streamlit command
 
-st.title("Worst-Case Analysis for Feature Rollouts")
+st.title("Tensor Data Platform")
 
 st.markdown("*Check out the [article](https://www.crosstab.io/articles/staged-rollout-analysis) for a detailed walk-through!*")
 x = st.slider('x')
