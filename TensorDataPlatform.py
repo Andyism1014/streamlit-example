7183684436993da5409789e0030f8598fc64ae0d
Andyism1014 <94820845+Andyism1014@users.noreply.github.com>
2021-12-01 12:03:31 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -16,13 +16,13 @@ left,right=st.columns(2)
 #huobi
 r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=100&symbol=latusdt")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
-huobi=pd.DataFrame({"t":a["id"],"symbol":"huobi","Volume":a["amount"]}).iloc[::-1]
+huobi=pd.DataFrame({"t":a["id"],"symbol":"huobi","Volume":a["amount"]})
 huobi["t"]=pd.to_datetime(huobi["t"],unit="s")
 
 #okex
 r=requests.get("https://www.okex.com/api/v5/market/history-candles?instId=LAT-USDT&bar=1D")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist(),columns=["id","open","high","low","close","vol","volCcy"])
-okex=pd.DataFrame({"t":a["id"],"symbol":"okex","Volume":a["vol"]}).iloc[::-1]
+okex=pd.DataFrame({"t":a["id"],"symbol":"okex","Volume":a["vol"]})
 okex["t"]=pd.to_datetime(okex["t"],unit="ms")
 #concat
 Volume=pd.concat([huobi,okex],ignore_index=True)
