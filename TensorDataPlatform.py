240b259aebde8abf34a7f8d137ff0950940ded12
Andyism1014 <94820845+Andyism1014@users.noreply.github.com>
2021-12-01 14:51:30 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -9,20 +9,19 @@ st.set_page_config(layout="wide")  # this needs to be the first Streamlit comman
 
 st.title("Tensor Data Platform")
 #LAT
-#huobi
 r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=100&symbol=latusdt")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
 huobi=pd.DataFrame({"t":a["id"],"symbol":"huobi","Volume":a["amount"]}).iloc[::-1]
 huobi["t"]=pd.to_datetime(huobi["t"],unit="s")
 #okex
 r=requests.get("https://www.okex.com/api/v5/market/history-candles?instId=LAT-USDT&bar=1D")
-b=pd.DataFrame(pd.read_json(r.text)["data"].tolist(),columns=["id","open","high","low","close","vol","volCcy"])
-okex=pd.DataFrame({"t":b["id"],"symbol":"okex","Volume":b["vol"]}).iloc[::-1]
+a=pd.DataFrame(pd.read_json(r.text)["data"].tolist(),columns=["id","open","high","low","close","vol","volCcy"])
+a["vol"]=pd.to_numeric(a["vol"])
+okex=pd.DataFrame({"t":a["id"],"symbol":"okex","Volume":a["vol"]}).iloc[::-1]
 okex["t"]=pd.to_datetime(okex["t"],unit="ms")
-#concat
+#combine
 Volume=pd.concat([huobi,okex])
-#concat
-c=pd.concat([huobi,okex])
+
 
 st.header("huobi")
 st.write(huobi)
@@ -30,6 +29,6 @@ st.write(huobi)
 st.header("okex")
 st.write(okex)
 
-st.header("c")
-st.write(c)
+st.header("Volume")
+st.write(Volume)
 
