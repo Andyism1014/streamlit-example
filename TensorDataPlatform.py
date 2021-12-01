72bd71a8553aee944a79be6e956e14509eec3ef2
Andyism1014 <94820845+Andyism1014@users.noreply.github.com>
2021-12-01 14:21:48 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -10,12 +10,12 @@ st.set_page_config(layout="wide")  # this needs to be the first Streamlit comman
 st.title("Tensor Data Platform")
 #LAT
 #huobi
-r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=50&symbol=latusdt")
+r=requests.get("https://api.huobi.pro/market/history/kline?period=1day&size=100&symbol=latusdt")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist())
 huobi=pd.DataFrame({"t":a["id"],"symbol":"huobi","Volume":a["amount"]}).iloc[::-1]
 huobi["t"]=pd.to_datetime(huobi["t"],unit="s")
 #okex
-r=requests.get("https://www.okex.com/api/v5/market/history-candles?instId=LAT-USDT&bar=1D&limit=50")
+r=requests.get("https://www.okex.com/api/v5/market/history-candles?instId=LAT-USDT&bar=1D")
 a=pd.DataFrame(pd.read_json(r.text)["data"].tolist(),columns=["id","open","high","low","close","vol","volCcy"])
 okex=pd.DataFrame({"t":a["id"],"symbol":"okex","Volume":a["vol"]}).iloc[::-1]
 okex["t"]=pd.to_datetime(okex["t"],unit="ms")
