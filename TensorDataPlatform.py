741485ec07c6543d8da0daf0c4c9aa1196dcf67d
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-06 13:20:08 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -40,37 +40,38 @@ if options == 'Portfolio Information':
 if options == 'ETF Information':
     set_ETF()
 if options=='Categories Information':
-    components.html(
-        """
-    <!-- TradingView Widget BEGIN -->
-    <div class="tradingview-widget-container">
-    <div id="tradingview_a94ef"></div>
-    <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/symbols/BTCUSDT/?exchange=BINANCE" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT Chart</span></a> by TradingView</div>
-    <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-    <script type="text/javascript">
-    new TradingView.widget(
-    {
-    "width": 980,
-    "height": 610,
-    "symbol": "BINANCE:BTCUSDT",
-    "interval": "D",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "1",
-    "locale": "in",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "allow_symbol_change": true,
-    "container_id": "tradingview_a94ef"
-    }
-    );
-    </script>
-    </div>
-    <!-- TradingView Widget END -->
+    with st.container():
+        components.html(
+            """
+        <!-- TradingView Widget BEGIN -->
+        <div class="tradingview-widget-container">
+        <div id="tradingview_a94ef"></div>
+        <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/symbols/BTCUSDT/?exchange=BINANCE" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT Chart</span></a> by TradingView</div>
+        <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+        <script type="text/javascript">
+        new TradingView.widget(
+        {
+        "width": 980,
+        "height": 610,
+        "symbol": "BINANCE:BTCUSDT",
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "in",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "tradingview_a94ef"
+        }
+        );
+        </script>
+        </div>
         <!-- TradingView Widget END -->
-        """  ,
-    height=1350, width=1800
-    )
+            <!-- TradingView Widget END -->
+            """  ,
+        height=1080, width=1920
+        )
 
 
 
