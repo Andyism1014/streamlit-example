258bad3a72b5dbf257f6967ec3406d72ef56ad2e
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-06 10:09:02 +0800

    Update Portfolio2.py

--- Portfolio2.py
+++ Portfolio2.py
@@ -123,12 +123,18 @@ def PaintVP(x):
 
 
 def set_Portfolio2():
-    st.header("LAT Consolidated Volume")
-    st.write(PaintVP(getinfor("lat"))[0])
-    st.write(PaintVP(getinfor("lat"))[1])
-    st.header("CKB Consolidated Volume")
-    st.write(PaintVP(getinfor("CKB"))[0])
-    st.write(PaintVP(getinfor("CKB"))[1])
-    st.header("KLAY Consolidated Volume")
-    st.write(PaintVP(getinfor("KLAY"))[0])
-    st.write(PaintVP(getinfor("KLAY"))[1])
+    option = st.selectbox(("Home",'LAT Consolidated Volume', 'CKB Consolidated Volume', 'KLAY Consolidated Volume'))
+    if option=="Home":
+        st.header("Home")
+    if option=='LAT Consolidated Volume':
+        st.header("LAT Consolidated Volume")
+        st.write(PaintVP(getinfor("lat"))[0])
+        st.write(PaintVP(getinfor("lat"))[1])
+    if option=='CKB Consolidated Volume':
+        st.header("CKB Consolidated Volume")
+        st.write(PaintVP(getinfor("CKB"))[0])
+        st.write(PaintVP(getinfor("CKB"))[1])
+     if option=='KLAY Consolidated Volume':
+        st.header("KLAY Consolidated Volume")
+        st.write(PaintVP(getinfor("KLAY"))[0])
+        st.write(PaintVP(getinfor("KLAY"))[1])
