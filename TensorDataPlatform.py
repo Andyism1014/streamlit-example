8731947e2fdc1e4471136e4afd73b2e74f583205
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-06 13:46:34 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -5,7 +5,7 @@ from PIL import Image
 from ETF import *
 from Portfolio2 import *
 import time
-import streamlit.components.v1 as components
+
 
 im = Image.open("logo.png")
 st.set_page_config(page_title='Tensor Data Platform',  layout='wide', page_icon=im)  # this needs to be the first Streamlit command
@@ -40,39 +40,6 @@ if options == 'Portfolio Information':
 if options == 'ETF Information':
     set_ETF()
 if options=='Categories Information':
-    components.html(
-        """
-    <!-- TradingView Widget BEGIN -->
-    <div class="tradingview-widget-container">
-    <div id="tradingview_35ad8"></div>
-    <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/ETHUSDT/?exchange=BINANCE" rel="noopener" target="_blank"><span class="blue-text">ETHUSDT Chart</span></a> by TradingView</div>
-    <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-    <script type="text/javascript">
-    new TradingView.widget(
-    {
-    "autosize": true,
-    "symbol": "BINANCE:ETHUSDT",
-    "interval": "D",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "1",
-    "locale": "en",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "hide_top_toolbar": true,
-    "hide_legend": true,
-    "save_image": false,
-    "show_popup_button": true,
-    "popup_width": "1000",
-    "popup_height": "650",
-    "container_id": "tradingview_35ad8"
-    }
-    );
-    </script>
-    </div>
-    <!-- TradingView Widget END -->
-        """  
-    )
 
 
 
