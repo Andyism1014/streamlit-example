26b592576baadd15a6385d09fab1617c3e50c10d
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-07 10:33:52 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -12,9 +12,10 @@ im = Image.open("logo.png")
 st.set_page_config(page_title='Tensor Data Platform',  layout='wide', page_icon=im)  # this needs to be the first Streamlit command
 
 
-
-st.image('logo.png', width=200)
-st.title('Tensor Data Platform')
+t1, t2 = st.columns((0.07,1))
+t1.image('logo.png', width=200)
+t2.title('Tensor Data Platform')
+t2.markdown("Tensor Investment Corporation | Proprietary trading and alternative investment firm")
 st.markdown('---')
 st.write('Navigation')
 #layout
