79b1ec5732fc368b169eee7048d6867337b13a61
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-07 14:40:48 +0800

    Update beta.py

--- beta.py
+++ beta.py
@@ -155,8 +155,12 @@ def PaintVP(x):
 def set_Portfolio():
   col1, col2= st.columns(2)
   with col1:
+    st.header("LAT price information")
     components.html("""
-    <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currencyid="9720" data-base="USD" data-secondary="" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-statsticker="true" data-stats="USD"></div>
+    <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js">
+    </script><div class="coinmarketcap-currency-widget" data-currencyid="9720" data-base="USD" data-secondary="" 
+    data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-statsticker="true" data-stats="USD">
+    </div>
     """)
   with col2:
     st.header("LAT Consolidated Volume")
