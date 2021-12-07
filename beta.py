0ed728272605bb4361cc69a9ace4feca12efb610
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-07 16:58:37 +0800

    Update beta.py

--- beta.py
+++ beta.py
@@ -170,7 +170,37 @@ def set_one(x):
     </div>
     """%(a),
     width=600)
-    
+    components.html("""
+    <!-- TradingView Widget BEGIN -->
+    <div class="tradingview-widget-container">
+      <div id="tradingview_17635"></div>
+      <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/LATUSDT/?exchange=HUOBI" rel="noopener" target="_blank"><span class="blue-text">LATUSDT Chart</span></a> by TradingView</div>
+      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+      <script type="text/javascript">
+      new TradingView.widget(
+      {
+      "width": 980,
+      "height": 610,
+      "symbol": "HUOBI:LATUSDT",
+      "interval": "60",
+      "timezone": "Etc/UTC",
+      "theme": "dark",
+      "style": "1",
+      "locale": "en",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "hide_legend": true,
+      "allow_symbol_change": true,
+      "save_image": false,
+      "container_id": "tradingview_17635"
+    }
+      );
+      </script>
+    </div>
+    <!-- TradingView Widget END -->
+    """,
+    width=600)
+
 
   with col2:
     st.header(x+" Consolidated Volume")
