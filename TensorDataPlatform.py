26858f26ca6691362f2878347390379b87fe1c0f
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-07 10:58:21 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -14,10 +14,6 @@ t1, t2 = st.columns((0.12,1))
 t1.image('logo.png', width=155)
 t2.title('Tensor Data Platform')
 t2.markdown("Tensor Investment Corporation | Proprietary trading and alternative investment firm")
-st.markdown(""" <style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-</style> """, unsafe_allow_html=True)
 st.write('Navigation')
 #layout
 col1, col2, col3,col4 = st.columns(4)
