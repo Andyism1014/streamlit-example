6b71137a6aac9ea63a2f9cba631ca4fe3400fc8c
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-08 15:14:52 +0800

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -12,9 +12,14 @@ def change_format(x):
                             })
     return s
 
+@st.experimental_memo
+def get_data():
+    df2=pd.read_csv("mas.csv",index_col="symbol")
+    return df2
+
 def set_beta():
     st.header("Market Information")
-    df2=pd.read_csv("mas.csv",index_col="symbol")
+    df2=get_data()
     name=["metrics.marketcap.current_marketcap_usd","metrics.marketcap.liquid_marketcap_usd","metrics.supply.y_plus10_issued_percent","metrics.supply.y_2050_issued_percent","metrics.market_data.price_usd",'metrics.market_data.real_volume_last_24_hours',"metrics.all_time_high.price","metrics.risk_metrics.sharpe_ratios.last_30_days","metrics.risk_metrics.sharpe_ratios.last_90_days","metrics.roi_data.percent_change_btc_last_1_week","metrics.roi_data.percent_change_btc_last_1_month","metrics.roi_data.percent_change_btc_last_3_months","metrics.risk_metrics.sharpe_ratios.last_1_year","metrics.risk_metrics.volatility_stats.volatility_last_30_days","metrics.risk_metrics.volatility_stats.volatility_last_90_days","metrics.risk_metrics.volatility_stats.volatility_last_1_year",'profile.general.overview.tags']
     df2=df2[name]
     df2=df2.rename(columns={
