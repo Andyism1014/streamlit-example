3439360dd538f53ce6e15edcfc5bd2c3345d09f5
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-09 11:36:58 +0800

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -239,7 +239,10 @@ def get_index(x):
 def set_Portfolio():
   get_index("BTCUSDT")
   get_index("ETHUSDT")
-  get_index("SPX")
+  get_index("US500")
+  get_index("NAS100USD")
+  get_index("000300")
+
 
   options = st.multiselect(
     'Select',
