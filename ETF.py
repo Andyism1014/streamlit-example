32ea4b60e402a1402a04cb61c34f1105ee397bca
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-10 18:10:00 +0800

    Update ETF.py

--- ETF.py
+++ ETF.py
@@ -8,6 +8,36 @@ from datetime import date
 from datetime import timedelta
 
 @st.experimental_memo(ttl=60*60*24)
+def get_netdata(x):
+  x=x.upper()
+  if x=="BTC":
+    list= ['binance','coinbase','huobi','kraken','okex',"kucoin","bitfinex","bithumb","gate.io","bitstamp"]
+  else:
+    list= ['binance','huobi','kraken','okex',"kucoin","bitfinex","bithumb","gate.io"]
+  for i in list:
+    if list.index(i)==0:
+      df=get_g_ex(x,"/v1/metrics/distribution/exchange_net_position_change",i,30)
+      df["symbol"]=i.capitalize() 
+    else:
+      b=get_g_ex(x,"/v1/metrics/distribution/exchange_net_position_change",i,30)
+      b["symbol"]=i.capitalize() 
+      df=pd.concat([df,b],ignore_index=True)
+  return df
+
+@st.experimental_memo
+def plot_netdata(df):
+  res=alt.Chart(df).mark_bar().encode(
+      x=alt.X("t:T",axis=alt.Axis(title="Time")),
+      y=alt.Y('v:Q',axis=alt.Axis(format="s",title="Net Position Change")),
+      color="symbol:N",
+      tooltip=[alt.Tooltip('t:T', title='Time'),alt.Tooltip('v:Q', title='Net Position Change',format="s"),alt.Tooltip('symbol:N', title='Net Position Exchange')]).properties(
+        width=700,
+        height=350
+    ).interactive(bind_y=False)
+  return res
+
+
+@st.experimental_memo(ttl=60*60*24)
 def getETF():
   #get update time:
   r=requests.get("https://www.oklink.com/api/oklink/v1/eth/datamaster/market/201",headers={"x-apiKey":"962feef7-6c1d-4c49-9ca2-dfc9a9d438bc"},params={
@@ -91,6 +121,9 @@ def set_ETF():
   with t2:
     st.header("ETH Institutions")
     st.write(getETF()[0])
+    st.write(plot_netdata(get_netdata("ETF")))
   with t1:
     st.header("BTC Institutions")
     st.write(getETF()[1])
+    st.write(plot_netdata(get_netdata("BTC")))
+
