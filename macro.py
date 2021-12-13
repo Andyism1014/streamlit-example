aa21317a62c8adc3a032986eac56f8c31b1f2905
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-13 17:52:50 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -12,4 +12,4 @@ import base64
 
 def main():
     components.iframe("https://studio.glassnode.com/workbench/4e473fa5-6bbb-4a50-7532-39677ab4c343",
-    height=550,width=700,scrolling=True)
+    height=550,width=1500,scrolling=True)
