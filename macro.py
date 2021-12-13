e2f2c813ec37c0c4eb589a6329d8d8a63fcb2278
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-13 16:49:32 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -7,10 +7,8 @@ import streamlit.components.v1 as components
 
 
 
-
-
-
 def main():
+    st.write("try")
     st.markdown("""
     <embed src="https://drive.google.com/viewerng/
     viewer?embedded=true&url=https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" width="400" height="400">
