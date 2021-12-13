fd0a237d5211f718d2bef179713f5efb4dc008f2
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-13 18:21:19 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -20,4 +20,4 @@ def main():
      was the season of Light, it was the season of Darkness, it
      was the spring of hope, it was the winter of despair, (...)
      ''')
-    st.write('Sentiment:', run_sentiment_analysis(txt))
+    st.write('Sentiment:', txt)
