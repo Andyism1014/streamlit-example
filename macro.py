93986e86b87607a4e8718e7aca53c5540a2ddd8a
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-13 17:18:51 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -7,15 +7,8 @@ import streamlit.components.v1 as components
 import base64
 
 
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-          base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+
 
 
 def main():
-    uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
-    if uploaded_file is not None:
-        st.write(uploaded_file)
     st.markdown("https://studio.glassnode.com/workbench/30ce7aa4-89bb-4194-41d9-399b4c0cfced",unsafe_allow_html=True)
