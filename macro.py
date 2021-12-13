2a98d6277d07ff5b6e4df012e7c0a03e6a64a67c
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-13 17:34:18 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -12,6 +12,6 @@ import base64
 
 def main():
     st.write(
-        f'<iframe src="https://www.google.com"></iframe>',
+        f'<iframe src="https://studio.glassnode.com/trading_view"></iframe>',
         unsafe_allow_html=True,
     )
