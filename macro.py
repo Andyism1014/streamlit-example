a8fed85336d2d347328b9ba08a87903b5e2f1df8
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-13 16:50:53 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -9,9 +9,7 @@ import streamlit.components.v1 as components
 
 def main():
     st.write("try")
-    st.markdown("""
-    <embed src="https://drive.google.com/viewerng/
-    viewer?embedded=true&url=https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" width="400" height="400">
-    """, unsafe_allow_html=True)
-
+    uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+    if uploaded_file is not None:
+        st.write(uploaded_file)
 
