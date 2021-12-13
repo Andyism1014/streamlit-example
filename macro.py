802bb5c2deef363a76702b93ebaebdc318242683
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-13 16:53:21 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -4,12 +4,14 @@ import pandas as pd
 import requests
 import streamlit as st
 import streamlit.components.v1 as components
-
+import base64
 
 
 def main():
     st.write("try")
     uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
     if uploaded_file is not None:
-        st.write(uploaded_file)
+        base64_pdf = base64.b64encode(uploaded_file.read()).decode('utf-8')
+        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">' 
+        st.markdown(pdf_display, unsafe_allow_html=True)
 
