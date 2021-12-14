3a43cff78278e4b9d21763cef5eadfeb969b13fe
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-14 12:47:34 +0800

    刚刚

--- macro.py
+++ macro.py
@@ -13,14 +13,11 @@ import base64
 def main():
     components.iframe("https://studio.glassnode.com/workbench/4e473fa5-6bbb-4a50-7532-39677ab4c343",
     height=1070,width=1700)
-    txt = st.text_area('Text to analyze', '''
+    st.text_area('Text to analyze', '''
      It was the best of times, it was the worst of times, it was
      the age of wisdom, it was the age of foolishness, it was
      the epoch of belief, it was the epoch of incredulity, it
      was the season of Light, it was the season of Darkness, it
      was the spring of hope, it was the winter of despair, (...)
-     ''')
-    st.write(txt)
-    file1 = open('read.txt', 'w')
-    print(file1.read()) 
-    file1.close()
+     ''',key="gg")
+    st.write(gg)
