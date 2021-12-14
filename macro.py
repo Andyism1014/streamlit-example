ecae8d7f6975ea0b722a2be91f9f38b74f5e769d
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-14 14:53:18 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -4,14 +4,13 @@ import pandas as pd
 import requests
 import streamlit as st
 import time
-from streamlit_quill import st_quill
+
 
 
 
 
 def main():
-    content = st_quill()
-    content
+    st.write("gg")
 
 
 
