ca321d91cdc78776658aea8d664c907181d53c6a
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-14 13:34:06 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -10,14 +10,11 @@ import time
 
 
 def main():
-    placeholder = st.empty()
-    start_button = st.empty()
-    if start_button.button('Start',key='start'):
-        start_button.empty()
-        if st.button('Stop',key='stop'):
-            pass
-        while True:
-            st.write("gg")
-            time.sleep(0.5)
+    txt = st.text_area('Text to analyze', '''
+    ss
+     ''')
+    if st.button('Start',key='start'):
+        st.write(txt)
+
 
 
