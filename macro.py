02506536648cfa12a6923269145b83984a04b93f
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-14 13:31:21 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -14,10 +14,10 @@ def main():
     start_button = st.empty()
     if start_button.button('Start',key='start'):
         start_button.empty()
-    if st.button('Stop',key='stop'):
-        pass
-    while True:
-        st.write("gg")
-        time.sleep(0.5)
+        if st.button('Stop',key='stop'):
+            pass
+        while True:
+            st.write("gg")
+            time.sleep(0.5)
 
 
