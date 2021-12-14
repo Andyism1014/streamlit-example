97df881ae11f93335f982c760b27fe482e321fc3
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-14 13:30:35 +0800

    Update macro.py

--- macro.py
+++ macro.py
@@ -3,13 +3,21 @@ import math
 import pandas as pd
 import requests
 import streamlit as st
-
+import time
 
 
 
 
 
 def main():
-    st.write("gg")
+    placeholder = st.empty()
+    start_button = st.empty()
+    if start_button.button('Start',key='start'):
+        start_button.empty()
+    if st.button('Stop',key='stop'):
+        pass
+    while True:
+        st.write("gg")
+        time.sleep(0.5)
 
 
