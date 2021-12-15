b0429ff30dd09fc6a5df19af219453fdf8d53c39
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-15 10:35:25 +0800

    Update On_Chain.py

--- On_Chain.py
+++ On_Chain.py
@@ -61,7 +61,7 @@ def show_cp(dfd,dfp):
     text
   ).resolve_scale(
           y ='independent').properties(
-      width=1600, height=1300
+      width=1600, height=1000
   ).interactive(bind_y=False)
   return res
 
