b4bdb192676bd72fd6ce7f5649891df5e28f02a8
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-16 10:54:24 +0800

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -250,7 +250,7 @@ def set_Portfolio():
   </div>
   <!-- TradingView Widget END -->
   """,
-    height=30)
+    height=50)
   st.header("Portfolio Information")
   options = ['LAT', 'CKB', 'KLAY']
   for i in options:
