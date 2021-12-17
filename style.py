9d1434d20d6d0f7c7fedfe5aba9f1cb0b8699a64
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-17 13:59:43 +0800

    Update style.py

--- style.py
+++ style.py
@@ -11,11 +11,11 @@ def shishikan():
     f"""
     <div class="conteudo" id="navbar">
     <a>&nbsp;</a>
-    <a href="{urlpath}?page=Inicio">Início</a>
-    <a href="{urlpath}?page=Metodologia">Modelos, limitações e fontes</a>
-    <a class="active" href="{urlpath}?page=Quem-Somos">Quem somos?</a>
-    <a href="{urlpath}?page=Estudo-Vacinacao">Estudo Vacinação</a>
-    <a href="{urlpath}?page=Vacinometro">Vacinômetro</a>
+    <a href="{https://farolcovid.coronacidades.org/}?page=Inicio">Início</a>
+    <a href="{https://farolcovid.coronacidades.org/}?page=Metodologia">Modelos, limitações e fontes</a>
+    <a class="active" href="{https://farolcovid.coronacidades.org/}?page=Quem-Somos">Quem somos?</a>
+    <a href="{https://farolcovid.coronacidades.org/}?page=Estudo-Vacinacao">Estudo Vacinação</a>
+    <a href="{https://farolcovid.coronacidades.org/}?page=Vacinometro">Vacinômetro</a>
     </div>
     """,
     unsafe_allow_html=True,
