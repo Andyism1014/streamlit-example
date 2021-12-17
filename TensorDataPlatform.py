02203daebcd4f1822cbd105d9922431f9736c629
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-17 16:18:39 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -24,7 +24,7 @@ if option=="Beta":
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
     st.markdown("""
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #1d1f1d;">
-    <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Tensor Data Platform</a>
+    <a class="navbar-brand"  target="_blank">Tensor Data Platform</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
