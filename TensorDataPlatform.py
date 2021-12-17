ace502eee129673772efa1a174049949702002c7
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-17 17:05:28 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -1,19 +1,15 @@
 from os import strerror, write
 import pandas as pd
 import streamlit as st
-from PIL import Image
 from Portfolio import *
 from On_Chain import *
 import time
 
 
-im = Image.open("logo.png")
+
 st.set_page_config(page_title='Tensor Data Platform',  layout='wide', page_icon=im)  # this needs to be the first Streamlit command
 
-t1, t2 = st.columns((0.07,1))
-t1.image('logo.png', width=100)
-t2.title('Tensor Data Platform')
-option = st.selectbox('Navigation',('Portfolio Information', 'On-Chain Data', 'Beta'))
+
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""
