d664fcd3d0ff0061649cea2d1beae01f080ef724
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-17 16:23:29 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -29,16 +29,6 @@ if option=="Beta":
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-        <li class="nav-item active">
-            <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank">Twitter</a>
-        </li>
         </ul>
     </div>
     </nav>
