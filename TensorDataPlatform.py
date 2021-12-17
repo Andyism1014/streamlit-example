507b06e92c4beab44c859c2827a91763013f7eca
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-17 16:44:13 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -20,7 +20,7 @@ st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" >
 <a class="navbar-brand" >
 <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-<a class="navbar-brand" href="#">
+
 Tensor Data Platform
 </a>
 </nav>
