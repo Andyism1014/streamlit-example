189af6d458a586bf099122b513467c80b972b743
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-17 14:26:19 +0800

    Update style.py

--- style.py
+++ style.py
@@ -7,17 +7,25 @@ import os
 
 def shishikan():
     st.write(
-        """
-        <div>
-                <div class="base-wrapper">
-                        <div>
-                            <span>Utilizando dados inéditos sobre os grupos prioritários para vacinação da Covid-19, obtidos pela Lei de Acesso à Informação, nós da Impulso Gov, projetamos quando podemos controlar a pandemia no Brasil.<br><br></span>
-                            <span>A partir da análise das informações disponíveis publicamente sobre produção nacional de vacina e dos acordos do Brasil para importação, e considerando quantas pessoas estão em cada grupo prioritário nos municípios brasileiros, eliminando a dupla-contagem, foi possível apontar três cenários para o avanço da vacinação no país.<br><br></span>
-                            <span><b>Os números mostram que, no cenário intermediário, que parece mais provável agora, a vacinação dos grupos prioritários seria possível até o fim de abril.</b> Já no pior cenário, em que o país só consiga alcançar metade da sua capacidade de produção de vacinas, todas as pessoas integrantes de grupos prioritários no Brasil estariam imunizadas até o fim de julho.<br><br></span>
-                            <span>O estudo, coordenado por Marco Brancher, especialista em dados e saúde da Impulso Gov, também projeta como a vacinação pode impactar na redução de óbitos no Brasil.<br><br></span>
-                        </div>
-                        <embed src="https://coronacidades.org/wp-content/uploads/2021/04/2020.04.12-Apresentac%CC%A7a%CC%83o-Dados-Vacinac%CC%A7a%CC%83o.pdf" width="100%" height="550">
+        f"""
+        <div class="base-wrapper" style="background-color:#0090A7;">
+            <div class="hero-wrapper">
+                <div class="hero-container" style="width:45%;">
+                    <div class="hero-container-content">
+                        <span class="subpages-container-product white-span">Vacinômetro</span>
+                        <span class="subpages-subcontainer-product white-span">Veja a evolução da vacinação em sua cidade ou estado! </span>
+                        <span class="subpages-container-subtitle white-span">Acompanhe e compare as informações mais atualizadas sobre a vacinação nos municípios do Brasil.</span>
+                    </div>
                 </div>
+                <div class="subpages-container-image">   
+                    <img style="width: 100%;" src="https://i.imgur.com/w5yVANW.png"/>
+                </div>
+            </div><br>
+        </div>
+        <div class="base-wrapper">
+            <span>  Aguarde o carregamento da tabela abaixo.</span>
+            <br><br>
+            <embed src="https://helper.coronacidades.org/vacinatable" width="100%" height="550">
         </div>
         """,
         unsafe_allow_html=True,
