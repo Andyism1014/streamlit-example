64e7a555884d8597b62b6d76ee3a7ee06a782c5e
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-17 14:01:31 +0800

    Update style.py

--- style.py
+++ style.py
@@ -4,19 +4,30 @@ import requests
 import streamlit as st
 import time
 import streamlit.components.v1 as components
-
+import os
 
 def shishikan():
+    """ 
+    This is a function that returns the "Footer" session 
+    
+    """
+    if os.getenv("IS_HEROKU") == "TRUE":
+        urlpath = os.getenv("urlpath")
+    elif os.getenv("IS_DEV") == "TRUE":
+        urlpath = 'http://localhost:8501/'
+    else:
+        urlpath = 'https://farolcovid.coronacidades.org/'
+
     st.write(
-    f"""
-    <div class="conteudo" id="navbar">
-    <a>&nbsp;</a>
-    <a href="{https://farolcovid.coronacidades.org/}?page=Inicio">Início</a>
-    <a href="{https://farolcovid.coronacidades.org/}?page=Metodologia">Modelos, limitações e fontes</a>
-    <a class="active" href="{https://farolcovid.coronacidades.org/}?page=Quem-Somos">Quem somos?</a>
-    <a href="{https://farolcovid.coronacidades.org/}?page=Estudo-Vacinacao">Estudo Vacinação</a>
-    <a href="{https://farolcovid.coronacidades.org/}?page=Vacinometro">Vacinômetro</a>
-    </div>
-    """,
+        f"""
+        <div class="conteudo" id="navbar">
+        <a>&nbsp;</a>
+        <a href="{urlpath}?page=Inicio">Início</a>
+        <a class="active" href="{urlpath}?page=Metodologia">Modelos, limitações e fontes</a>
+        <a href="{urlpath}?page=Quem-Somos">Quem somos?</a>
+        <a href="{urlpath}?page=Estudo-Vacinacao">Estudo Vacinação</a>
+        <a href="{urlpath}?page=Vacinometro">Vacinômetro</a>
+        </div>
+        """,
     unsafe_allow_html=True,
     )
