6d1e335b30e032d23cc7857e1b403abbe0192b87
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-17 16:44:35 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -18,9 +18,8 @@ option = st.selectbox('Navigation',('Portfolio Information', 'On-Chain Data', 'B
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" >
-<a class="navbar-brand" >
+<a class="navbar-brand" href="#">
 <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-
 Tensor Data Platform
 </a>
 </nav>
