5c8d774df3d565ebc1444104ee11ba81d0cd9e93
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-17 16:23:58 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -25,12 +25,7 @@ if option=="Beta":
     st.markdown("""
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #1d1f1d;">
     <a class="navbar-brand"  target="_blank">Tensor Data Platform</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        </ul>
-    </div>
+
     </nav>
     """, unsafe_allow_html=True)
 
