9fa782818a1588b54d2c521033cec66880ab1011
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-20 15:20:53 +0800

    Update style.py

--- style.py
+++ style.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-_radio_button = components.declare_component(
-    "radio_button", url="http://localhost:3001",
-)
+page_list = ['Portfolio Information', 'On-Chain Data', 'Beta']
+query_params = st.experimental_get_query_params()
+# Query parameters are returned as a list to support multiselect.
+# Get the first item in the list if the query parameter exists.
+default = int(query_params["option"][0]) if "option" in query_params else 0
+
+option = st.sidebar.radio("",page_list,index = default)
+if option:
+    query_params["option"] = page_list.index(option)
+    st.experimental_set_query_params(**query_params)
 
 
-def custom_radio_button(label, options, default, key=None):
-    return _radio_button(label=label, options=options, default=default, key=key)
-
-
-result = custom_radio_button(
-    "How many bats?",
-    options=["one bat", "TWO bats", "THREE bats", "FOUR BATS! ah ah ah!"],
-    default="one bat",
-)
-st.write("This many: %s" % result)
-
+if option=="Portfolio Information":
+    set_Portfolio()
+if option=="On-Chain Data":
+    main()
