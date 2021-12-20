5262d6697863cba381a836d9a45c430b4060a660
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-20 14:35:19 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -20,15 +20,7 @@ Tensor Data Platform
 """,unsafe_allow_html=True)
 
 
-page_list = ['Portfolio Information', 'On-Chain Data', 'Beta']
-query_params = st.experimental_get_query_params()
-# Query parameters are returned as a list to support multiselect.
-# Get the first item in the list if the query parameter exists.
-default = int(query_params["option"][0]) if "option" in query_params else 0
-option = st.radio("",page_list,index = default)
-if option:
-    query_params["option"] = page_list.index(option)
-    st.experimental_set_query_params(**query_params)
+
 
 #layout
 st.write("""
