1bc1e450652c2875ab08bd1a364d00b6cfb7684c
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-20 18:27:10 +0800

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -124,9 +124,7 @@ def PaintVP(x,MA):
   a=alt.Chart(x[1]).mark_line().encode(
       x=alt.X(per,axis=alt.Axis(title=None)),
       y=alt.Y('Price:Q',scale=alt.Scale(zero=False)))
-  b=alt.Chart(V).mark_area(opacity=0.6).transform_window(
-    Volume='mean(Volume)',
-    frame=[0,7]).encode(
+  b=alt.Chart(V).mark_area(opacity=0.6).encode(
       x=alt.X(per,axis=alt.Axis(title=None)),
       y=alt.Y("Volume:Q",axis=alt.Axis(format="s")),
       color="symbol:N")
