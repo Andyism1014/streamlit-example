3ac02df2146eae7380411dcadbaa20bec43cd1fe
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-20 14:21:56 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -26,8 +26,7 @@ query_params = st.experimental_get_query_params()
 # Get the first item in the list if the query parameter exists.
 default = int(query_params["option"][0]) if "option" in query_params else 0
 option = st.radio("",page_list,index = default)
-if option:
-    st.experimental_set_query_params(option=page_list.index(option))
+st.experimental_set_query_params(option=page_list.index(option))
 #layout
 if option=="Portfolio Information":
     set_Portfolio()
