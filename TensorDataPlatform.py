f22711e8bf05e91f76535394491e6558ac3011a4
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-20 14:36:24 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -30,20 +30,20 @@ query parameters. With these query params, you can bookmark or share your app
 in various states. Thanks [@zhaoooyue](https://github.com/zhaoooyue) for the
 contribution!
 """)
-with st.echo("below"):
-    radio_list = ['Eat', 'Sleep', 'Both']
-    query_params = st.experimental_get_query_params()
 
-    # Query parameters are returned as a list to support multiselect.
-    # Get the first item in the list if the query parameter exists.
-    default = int(query_params["activity"][0]) if "activity" in query_params else 0
-    activity = st.radio(
-        "What are you doing at home during quarantine?",
-        radio_list,
-        index = default
-    )
-    if activity:
-        st.experimental_set_query_params(activity=radio_list.index(activity))
+radio_list = ['Eat', 'Sleep', 'Both']
+query_params = st.experimental_get_query_params()
+
+# Query parameters are returned as a list to support multiselect.
+# Get the first item in the list if the query parameter exists.
+default = int(query_params["activity"][0]) if "activity" in query_params else 0
+activity = st.radio(
+    "What are you doing at home during quarantine?",
+    radio_list,
+    index = default
+)
+if activity:
+    st.experimental_set_query_params(activity=radio_list.index(activity))
 
 
 
