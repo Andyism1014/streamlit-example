4f2f37713d7a06cb5d923d9f7f5444d8c30d9451
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-20 11:28:27 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -9,12 +9,9 @@ import time
 im = Image.open("logo.png")
 st.set_page_config(page_title='Tensor Data Platform',  layout='wide', page_icon=im)  # this needs to be the first Streamlit command
 
-def tabs(default_tabs = [], default_active_tab=0):
-        if not default_tabs:
-            return None
-        active_tab = st.radio("", default_tabs, index=default_active_tab, key='tabs')
-        child = default_tabs.index(active_tab)+1
-        st.markdown("""  
+
+
+st.markdown("""  
             <style type="text/css">
             div[role=radiogroup] > label > div:first-of-type, .stRadio > label {
                display: none;               
@@ -29,20 +26,10 @@ def tabs(default_tabs = [], default_active_tab=0):
                 border-radius: 4px 4px 0 0;
                 position: relative;
                 top: 1px;
-                }
-            div[role=radiogroup] label:nth-child(""" + str(child) + """) {    
-                background: #FFF !important;
-                border-bottom: 1px solid transparent;
-            }            
+                }           
             </style>
-        """,unsafe_allow_html=True)        
-        return active_tab
-
-active_tab = tabs(["Tab 1", "Tab 2", "Tab 3"])
-st.write(active_tab)
-
-
-option = st.selectbox("",('Portfolio Information', 'On-Chain Data', 'Beta'))
+        """,unsafe_allow_html=True)  
+option = st.radio("",('Portfolio Information', 'On-Chain Data', 'Beta'))
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" >
