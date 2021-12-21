55665591e79b3e7e2321e4ae91b5102a835b960e
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-21 15:52:17 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -38,12 +38,3 @@ if page=="On-Chain Data":
     main()
 if page=="Beta":
     st.title("Studio")
-    source = pd.DataFrame({
-        'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
-        'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
-    })
-
-    st.write(alt.Chart(source,width="container").mark_bar().encode(
-        x='a',
-        y='b'
-    ))
