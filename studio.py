ec2fbd7eb2a8a5f930d0000128bacf208dc4d37e
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-30 16:23:00 +0800

    ggg

--- studio.py
+++ studio.py
@@ -6,6 +6,7 @@ import streamlit.components.v1 as components
 import os
 from st_aggrid import AgGrid,GridOptionsBuilder,GridUpdateMode, DataReturnMode, JsCode
 from streamlit.proto.Button_pb2 import Button
+from datetime import datetime
 
 @st.experimental_memo(ttl=60*60*24)
 def get_g(symbol,addresses,intervel,currency):
@@ -7966,13 +7967,14 @@ def picture(l):
 
 @st.experimental_memo(ttl=60*60*24)
 def messariP(x):
+  today=datetime.today().strftime('%Y-%m-%d')
   url="https://data.messari.io/api/v1/assets/%s/metrics/price/time-series"%(x)
   header={ "x-messari-api-key": "77fd912b-7c49-449f-a808-d3755b9bb69a"}
-  r=requests.get(url,headers=header,params = {"interval":"1d","columns":"close"})
+  r=requests.get(url,headers=header,params = {"start":"2021-01-01","end":today,"interval":"1d","columns":"close"})
   df=pd.DataFrame(r.json()["data"]["values"])
   return df
 
-lay1=["SOL","BNB","ADA","MATIC","AVAX"]
+lay1=["SOL","BNB","ADA","MATIC","AVAX","LUNA"]
 def messari():
   ethP=messariP("ETH")
   fig=go.Figure()
@@ -7981,12 +7983,24 @@ def messari():
   for i in lay1:
     df=messariP(i)
     df[1]=df[1]/ethP[1]
+    df[1]=df[1].pct_change()
+    df[1]=df[1]+1
+    df[1]=df[1].cumprod()
     df[0]=pd.to_datetime(df[0], unit="ms").dt.date
     fig.add_trace(go.Scatter(
         x=df[0],
         y=df[1].rolling(slider).mean(),
         name=i
       ))
+  fig.update_layout(
+    legend=dict(
+    orientation="h",
+    yanchor="top",
+    y=-0.05,
+    xanchor="left",
+    x=0
+)
+  )
   st.plotly_chart(fig, use_container_width=True,config=config)
 
 
