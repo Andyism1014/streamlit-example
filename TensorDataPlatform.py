d6143058a7176f2789759710afc32266d210ae78
Andy <94820845+Andyism1014@users.noreply.github.com>
2021-12-30 18:30:02 +0800

    Update TensorDataPlatform.py

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -11,7 +11,7 @@ im = Image.open("logo.png")
 st.set_page_config(page_title='Tensor Data Platform',  layout='wide', page_icon=im)  # this needs to be the first Streamlit command
 
 menu_data = [
-    {'icon': "", 'label':"On-Chain Data"},
+    {'icon': "", 'label':"Metrics"},
     {'icon': "", 'label':"Studio"},
     {'icon': "", 'label':"Beta"},
     {'icon': "", 'label':"Other"},
@@ -32,27 +32,12 @@ menu_id = hc.nav_bar(
 
 if f"{menu_id}"=="Home":
     set_Portfolio()
-if f"{menu_id}"=="On-Chain Data":
+if f"{menu_id}"=="Metrics":
     dashbord2()
 if f"{menu_id}"=="Studio":
     tabletry()
 if f"{menu_id}"=="Other":
     set_ETF()
-if f"{menu_id}"=="Beta":
-    # Everything is accessible via the st.secrets dict:
-    st.write("DB username:", st.secrets["db_username"])
-    st.write("DB password:", st.secrets["db_password"])
-    st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
-
-    # And the root-level secrets are also accessible as environment variables:
-
-    import os
-
-    st.write(
-        "Has environment variables been set:",
-        os.environ["db_username"] == st.secrets["db_username"],
-)
-
 
     
 
