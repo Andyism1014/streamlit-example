2d9eef7dc695bd686b4f3b2a82039e024856c122
Andy <94820845+Andyism1014@users.noreply.github.com>
2022-01-04 11:35:18 +0800

    ggg

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -1,16 +1,21 @@
 import streamlit as st
-from PIL import Image
 import hydralit_components as hc
 from Portfolio import *
 from studio import *
 from ETF import *
+import streamlit_authenticator as stauth
 
 
+st.set_page_config(page_title='Tensor Data Platform',  layout='wide')  # this needs to be the first Streamlit command
 
-im = Image.open("logo.png")
-st.set_page_config(page_title='Tensor Data Platform',  layout='wide', page_icon=im)  # this needs to be the first Streamlit command
+names = ['Tensorcorp']
+usernames = ['tech@tensorcorp.com']
+passwords = ['12345678']
+
+hashed_passwords = stauth.hasher(passwords).generate()
 
 menu_data = [
+    {'icon': "", 'label':"Portfolio"},
     {'icon': "", 'label':"Data Overview"},
     {'icon': "", 'label':"Studio"},
     {'icon': "", 'label':"Beta"},
@@ -23,23 +28,33 @@ menu_id = hc.nav_bar(
     menu_definition=menu_data,
     override_theme=over_theme,
     home_name='Home',
-    hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
+    hide_streamlit_markers=True, #will show the st hamburger as well as the navbar now!
     sticky_nav=True, #at the top or not
     sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
 )
 
 
-
 if f"{menu_id}"=="Home":
-    set_Portfolio()
+    authenticator = stauth.authenticate(names,usernames,hashed_passwords,
+        'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+    name, authentication_status = authenticator.login('Login','main')
+    if st.session_state['authentication_status']:
+        st.header('Welcome *%s*' % (name))
+if f"{menu_id}"=="Portfolio":
+    if st.session_state['authentication_status']:
+        set_Portfolio()
 if f"{menu_id}"=="Data Overview":
-    dashbord2()
+    if st.session_state['authentication_status']:
+        dashbord2()
 if f"{menu_id}"=="Studio":
     tabletry()
 if f"{menu_id}"=="Beta":
     PerpOI()
 if f"{menu_id}"=="Other":
-    set_ETF()
+    if st.session_state['authentication_status']:
+        set_ETF()
+
+
 
     
 
