ef8edc5d595e0334d2f36966f2baf76a5039c3a2
Andy <94820845+Andyism1014@users.noreply.github.com>
2022-01-04 16:43:49 +0800

    gg

--- TensorDataPlatform.py
+++ TensorDataPlatform.py
@@ -51,8 +51,6 @@ if st.session_state['authentication_status']:
         dashbord2()
 if f"{menu_id}"=="Studio":
     tabletry()
-if f"{menu_id}"=="Beta":
-    picofdata()
 if f"{menu_id}"=="Other":
     set_ETF()
 
