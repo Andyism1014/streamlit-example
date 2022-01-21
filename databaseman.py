2b735fa6b6c2d0af9ae11108b4192b56b16b948d
Andy <94820845+Andyism1014@users.noreply.github.com>
2022-01-21 16:59:23 +0800

    gg

--- databaseman.py
+++ databaseman.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine
-from streamlit_autorefresh import st_autorefresh
 import requests
 
 st.set_page_config(page_title='Tensor DataBase Management', layout='wide')
